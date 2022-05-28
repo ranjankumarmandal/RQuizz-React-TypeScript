8352005b2a5e980a487b3cd97a203bff23a3491f
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-29 01:04:13 +0530

    passed all the props to child component QuestionCard.tsx from parent App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState([]);
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
-  const [score, setScore] = useState(0g);
+  const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
-  
+
   const startTrivia = () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -23,7 +25,14 @@ function App() {
       </button>
       <p className='score'>Score:</p>
       <p>Loading Questions...</p>
-      <QuestionCard />
+      <QuestionCard
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className='next' onClick={nextQuestion}>
         Next Question
       </button>
