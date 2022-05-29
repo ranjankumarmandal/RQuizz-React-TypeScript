b2664c7f97d64dc698bc319191906d201a905a36
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-29 18:07:28 +0530

    created GlobalStyle() inside file App.styles.ts

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,2 +1,22 @@
 import styled, { createGlobalStyle } from 'styled-components';
 import bgImage from './images/bgImage.jpg';
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        height: 100%;
+    }
+
+    body {
+        background-image: url(${bgImage});
+        background-size: cover;
+        margin: 0;
+        padding: 0 20px;
+        display: flex;
+        justify-content: center;
+    }
+
+    * {
+        box-sizing: border-box;
+        font-family: 'catamaran', sans-serif;
+    }
+`;
