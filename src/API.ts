75a2dceddef3398da10c8d9553082dc05982cc68
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-29 09:09:23 +0530

    created type Question with typescript interface in file API.ts

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,12 @@
+export interface Question {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answer: string[];
+  question: string;
+  type: string;
+}
+
 export enum Difficulty {
   EASY = 'easy',
   MEDIUM = 'medium',
