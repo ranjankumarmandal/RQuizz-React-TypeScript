a1a022131013f7e738aac4fca39f374ad039bf4b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-05-29 17:56:51 +0530

    added score value

--- src/App.tsx
+++ src/App.tsx
@@ -71,7 +71,7 @@ function App() {
           Start
         </button>
       ) : null}
-      {!gameOver && <p className='score'>Score:</p>}
+      {!gameOver && <p className='score'>Score: {score}</p>}
       {loading && <p>Loading Questions...</p>}
       {!loading && !gameOver && (
         <QuestionCard
